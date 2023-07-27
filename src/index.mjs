1c902d9d09cf2bba79eee2f79ebb902f051253fc
ja153903 <jaimeabbariao@gmail.com>
2023-07-27 12:29:30 -0400

    feat: updating with route register function

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,11 @@
 import express from "express";
 
-import { ROUTES } from "./lib/routes.mjs";
+import { registerRoutes } from "./lib/routes.mjs";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-Object.entries(ROUTES).forEach(([url, router]) => {
-  app.use(url, router);
-});
+registerRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
--- src/lib/routes.mjs
+++ src/lib/routes.mjs
@@ -1,7 +1,19 @@
 import { ACCOUNTS_V1 } from "../constants/routes.mjs";
 import accountsRouter from "../accounts/router.mjs";
 
-// NOTE: Register routes here with respective router
+/** @type {Record<string, import("express").Router>} */
 export const ROUTES = {
   [ACCOUNTS_V1]: accountsRouter,
 };
+
+/**
+ * `registerRoutes` makes sure that all our routers are
+ * registered with the Express app.
+ *
+ * @param {import("express").Express} app
+ */
+export function registerRoutes(app) {
+  Object.entries(ROUTES).forEach(([url, router]) => {
+    app.use(url, router);
+  });
+}
