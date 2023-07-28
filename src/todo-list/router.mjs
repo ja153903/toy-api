88381007845c5770a07aba423e79d2335d014739
ja153903 <jaimeabbariao@gmail.com>
2023-07-27 21:56:57 -0400

    feat: adding POST route

--- src/todo-list/router.mjs
+++ src/todo-list/router.mjs
@@ -1,5 +1,30 @@
 import { Router } from "express";
 
+import { CREATE } from "../constants/routes.mjs";
+import { addTodo } from "./services.mjs";
+
 const router = Router();
+
+router.post(CREATE, async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    res.status(400).json({
+      error: "The payload does not contain the right values",
+    });
+    return;
+  }
+
+  const todo = await addTodo(body);
+  if (todo === null) {
+    res.status(400).json({
+      error: "Could not create todo",
+    });
+    return;
+  }
+
+  res.status(200).json({
+    todo,
+  });
+});
 
 export default router;
--- src/todo-list/services.mjs
+++ src/todo-list/services.mjs
@@ -1,1 +1,17 @@
-export {};
+import db from "../lib/db.mjs";
+import { todos } from "./models.mjs";
+
+/**
+ * @typedef {{ title: string, task: string }} TodoPayload
+ * @param {TodoPayload} todoPayload
+ * @returns {Promise<unknown | null>}
+ */
+async function addTodo(todoPayload) {
+  const queryResult = await db.insert(todos).values(todoPayload).returning({
+    id: todos.id,
+  });
+
+  return queryResult ?? null;
+}
+
+export { addTodo };
